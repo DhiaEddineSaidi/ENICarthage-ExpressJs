60181238a5da729ac4d2103ec46692741bed0ffa
Saidi DhiaEddine <dhiasaidi.ensit@gmail.com>
2023-11-25 15:58:32 +0100

    ajout de mongodb

--- index.js
+++ index.js
@@ -1,34 +1,107 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
+const  express = require('express');
+
+const cors = require('cors')
+
+const  {connectToDb , getDb} = require('./db')
+
 const port = 3000
 
-app.use(cors())
+const app = express()
 
-me = {
-    nom : 'Saidi',
-    prenom : 'DhiaEddine',
-    age: 31 ,
-    dateNaissance : new Date(2023,11,29)
-}
+let db 
+
+connectToDb((err)=>{
+  if(!err){
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+  })
+  db = getDb()
+  }
+
+})
+
+app.use(cors())
+app.use(express.json())
+
+
+  app.route('/persons')
+    .get((req, res)=> {
+      db.collection('person').find().toArray().then(results=>{
+        console.log(results);
+        res.contentType('application/json');
+        res.status(200).json(results)
+      })
+    })
+
+    app.route('/person')
+    .post((req, res) => {
+      console.log(req.body);
+      db.collection('person').insertOne(req.body);
+      res.contentType('application/json');
+      res.status(201).json(req.body)
+    })
+
+    app.route('/person/:_id')
+    .get((req, res) => {
+      console.log(req.params);
+      db.collection('person').find(req.params).toArray().then(results => {
+        console.log(results);
+        res.contentType('application/json');
+        res.status(200).json(JSON.stringify(results))
+      })
+    })
+    // .put((req, res) => {
+    //   console.log(req.params);
+    //   db.collection('person').findOneAndUpdate({
+    //     _id: ObjectId(req.body._id)
+    //   }, {
+    //     $set: { nom: req.body.nom, prenom: req.body.prenom }
+    //   }).then(result => {
+    //     res.contentType('application/json');
+    //     res.status(200).json(JSON.stringify(result))
+    //   })
+    // })
+    // .delete((req, res) => {
+    //   console.log(req.body);
+    //   db.collection('person').deleteOne({
+    //     _id: ObjectId(req.body._id)
+    //   }).then(result => {
+    //     console.log("deleting Object with : ", req.body._id);
+    //     if (result.deletedCount === 0) {
+    //       res.contentType('application/json')
+    //       res.status(200).json(JSON.stringify({ status: req.body._id + " is deleted" }))
+    //     }
+    //   })
+    // })
+
+
+
+ const me = {
+     nom: 'Saidi',
+     prenom: 'DhiaEddine',
+     age: 31,
+     dateNaissance: new Date(2023, 11, 29)
+ }
 
 app.get('/me', (req, res) => {
     console.log(req)
     res.json(me)
 })
 
-app.post('/me', (req,res)=> {
-    try {
-        console.log(req)
-        Object.assign(me, req)
-        res.status(200).json('Done !!')    
-    } catch (error) {
-        res.status(500).json('could not update me !!')
-    }
-    
-        
-})
+// app.post('/me', async  (req,res)=> {
+//     const data = req.body    
+//     const client = new MongoClient("mongodb://127.0.0.1:27017");
+//           try {
+//             await  client.connect();
+//             const database = client.db('me-database'); // Choose a name for your database
+//             const collection = database.collection('person'); // Choose a name for your collection
+//             console.log(req)
+//             await collection.insertOne(data);
+//             res.status(201).json({ message: 'Data saved successfully!' });
+//           } catch (error) {
+//             res.status(500).json({ message: 'Something went wrong!' });
+//           } finally {
+//             await client.close();
+//           }   
+// })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
